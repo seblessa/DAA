1bb5ad46a157b8e5dd251ead6b6f0977190da842
Seblessa <sebastiaolessa2002@outlook.com>
2023-05-27 22:50:19 +0100

    working

--- pratica7/DAA025.java
+++ pratica7/DAA025.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class DAA025 {
@@ -7,40 +5,13 @@ public class DAA025 {
     static boolean[][] adj;    // Matriz de adjacencias
     static boolean[] visited;  // Que nos ja foram visitados?
 
-    public static String dfs(int v) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(v).append(" ");
+    static void dfs(int v) {
         visited[v] = true;
-        for (int i = 1; i <= n; i++) {
-            if (adj[v][i] && !visited[i]) {
-                sb.append(dfs(i));
-            }
-        }
-        return sb.toString().toString();
+        for (int i=1; i<=n; i++)
+            if (adj[v][i] && !visited[i])
+                dfs(i);
     }
 
-    public static int[] toIntlist(String str){
-        String[] arr_str = str.split(" ");
-
-        int[] arr = new int[arr_str.length];
-
-        for (int i=0;i< arr.length;i++){
-            arr[i]=Integer.parseInt(arr_str[i]);
-        }
-
-        return arr;
-    }
-
-
-    public static boolean check(int[][] m,int n){
-        for (int[] num0:m){
-            for (int num1:num0){
-                if (num1==0) return false;
-                if (num1==n) return true;
-            }
-        }
-        return false;
-    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -56,21 +27,9 @@ public class DAA025 {
         }
 
         int count=0;
-        boolean[] visited = new boolean[n];
-        boolean flag=true;
-
         for (int i=1;i<=n;i++){
-            int[] out = toIntlist(dfs(i));
-            for (int n:out){
-                if (!visited[n-1]){
-                    visited[n-1]=true;
-                    if (flag){
-                        count++;
-                        flag=false;
-                    }
-                }
-            }
-            flag=true;
+            if (!visited[i]) count++;
+            dfs(i);
         }
 
         System.out.println(count);
--- pratica7/DAA026.java
+++ pratica7/DAA026.java
@@ -1,2 +1,66 @@
+import java.util.Scanner;
+
 public class DAA026 {
+
+    static char ALIVE = '#';
+    static int L;
+    static int C;
+    static int counter;
+    static char[][] cells;
+    static boolean[][] visited;
+    static char[][] create_matrix(Scanner in) {
+        char[][] matrix = new char[L][C];
+
+        for (int i=0;i<L;i++){
+            String str = in.nextLine();
+            for (int j=0;j<C;j++) {
+                matrix[i][j] = str.charAt(j);
+            }
+        }
+        return matrix;
+    }
+
+    static void dfs(int i,int j) {
+        if (i>=0 && i<L && j>=0 && j<C){
+            if (!visited[i][j] && cells[i][j]==ALIVE){
+                visited[i][j] = true;
+                counter++;
+                dfs(i+1,j);
+                dfs(i+1,j+1);
+                dfs(i,j+1);
+                dfs(i-1,j);
+                dfs(i-1,j+1);
+                dfs(i-1,j-1);
+                dfs(i,j-1);
+                dfs(i+1,j-1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        for (int i=0;i<N;i++){
+            L = in.nextInt();
+            C = in.nextInt();
+            in.nextLine();
+
+            cells = create_matrix(in);
+            visited = new boolean[L][C];
+
+            int max_counter=0;
+            for (int n=0;n<L;n++){
+                for (int m=0;m<C;m++){
+                    if (!visited[n][m]) dfs(n,m);
+                    max_counter=Math.max(max_counter,counter);
+                    counter=0;
+                }
+            }
+
+            System.out.println(max_counter);
+
+        }
+    }
 }
